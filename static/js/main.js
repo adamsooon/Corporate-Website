b28e0685d52ab68dbbc94e53af35206ccdf06a47
Adam <king_4@o2.pl>
2016-06-15 12:25:16 +0200

    Restart and deleted height from section.  Improved footer

--- static/js/main.js
+++ static/js/main.js
@@ -16,6 +16,31 @@
             responsive: true,
             controls: false,
         });
+        $('.form-contact--name').blur(function () {
+        var a = $('.form-contact--name').val();
+            if (a.length > 0) {
+                $('.form-contact--name + label').addClass('valid');
+            } else {
+                $('.form-contact--name + label').removeClass('valid');
+            }
+        });
+        $('.form-contact--email').blur(function () {
+        var a = $('.form-contact--email').val();
+            if (a.length > 0) {
+                $('.form-contact--email + label').addClass('valid');
+            } else {
+                $('.form-contact--email + label').removeClass('valid');
+            }
+        });
+        $('.form-contact--message').blur(function () {
+        var a = $('.form-contact--message').val();
+            if (a.length > 0) {
+                $('.form-contact--message + label').addClass('valid');
+            } else {
+                $('.form-contact--message + label').removeClass('valid');
+            }
+        });
+
         window.CHANGE_ME.init();
     });
 }(jQuery, window, document));
